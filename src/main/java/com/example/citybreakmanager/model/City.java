cd8642fca9d9106dd602cce8f1851d997531a823
SKharel10 <sagarkharel001@gmail.com>
2026-01-02 21:57:20 +0000

    setup spring boot dev environment + created models for trip and cities

---
+++ src/main/java/com/example/citybreakmanager/model/City.java
@@ -0,0 +1,9 @@
+package com.example.citybreakmanager.model;
+
+public class City {
+    private int id;
+    private String name;
+    private String country;
+    private String details;
+}
+
---
+++ src/main/java/com/example/citybreakmanager/model/Trip.java
@@ -0,0 +1,11 @@
+package com.example.citybreakmanager.model;
+import java.time.LocalDate;
+
+public class Trip {
+    private Integer id;
+    private City city;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Integer rating;
+    private String personalNotes;
+}
