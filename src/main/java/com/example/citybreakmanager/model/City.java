65e13da95e76d6f0d4046d53132d9b2ee2449d9b
SKharel10 <sagarkharel001@gmail.com>
2026-01-04 01:14:33 +0000

    Tried using Lombok annotations to clean up code but they do not appear to work, will revert for now

--- src/main/java/com/example/citybreakmanager/model/City.java
+++ src/main/java/com/example/citybreakmanager/model/City.java
@@ -7,10 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class City {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +15,36 @@ public class City {
   private String name;
   private String country;
   private String details;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
+  }
 }
--- src/main/java/com/example/citybreakmanager/model/Trip.java
+++ src/main/java/com/example/citybreakmanager/model/Trip.java
@@ -8,10 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Trip {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +18,52 @@ public class Trip {
   private LocalDate endDate;
   private Integer rating;
   private String personalNotes;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public City getCity() {
+    return city;
+  }
+
+  public void setCity(City city) {
+    this.city = city;
+  }
+
+  public LocalDate getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+  }
+
+  public LocalDate getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+  }
+
+  public Integer getRating() {
+    return rating;
+  }
+
+  public void setRating(Integer rating) {
+    this.rating = rating;
+  }
+
+  public String getPersonalNotes() {
+    return personalNotes;
+  }
+
+  public void setPersonalNotes(String personalNotes) {
+    this.personalNotes = personalNotes;
+  }
 }
